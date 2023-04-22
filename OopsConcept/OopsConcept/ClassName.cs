0f2415cc3e2c8e63ded1a766868ab2fc0c2f8073
ronitshinde <rony.shinde753@gmail.com>
2023-04-22 08:39:59 +0530

    [Ronit] Add : UC1-Creating classes and objects

---
+++ OopsConcept/OopsConcept/ClassName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class ClassName
+    {
+        public void PrintName()
+        {
+            Console.WriteLine("Bridgelabz");
+        }
+    }
+}
---
+++ OopsConcept/OopsConcept/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassName name = new ClassName();
+            name.PrintName();
+            //It will hold the screen until the key is pressed.
+            Console.ReadKey();
+        }
+    }
+}
