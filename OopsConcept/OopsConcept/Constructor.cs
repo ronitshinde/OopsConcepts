14429dc888bed8270030b82fda2054e8213d4239
ronitshinde <rony.shinde753@gmail.com>
2023-04-23 17:55:18 +0530

    [Ronit] Add : UC4-Parameterised constructor

--- OopsConcept/OopsConcept/Constructor.cs
+++ OopsConcept/OopsConcept/Constructor.cs
@@ -10,9 +10,45 @@ namespace OopsConcept
     {
         //Constructor does not have any return type.
         //Name should be same as the class name.
+        //This is a non-parameterised constructor.
         public Constructor()
         {
             Console.WriteLine("Hello World");
+        }
+        //This is a parameterised constructor.
+        public string query;
+        public int transactionNum;
+        public char alphabet;
+        public Constructor(string name, int number, char character)
+        {
+            this.query = name;
+            Console.WriteLine(query);
+            transactionNum = number;
+            Console.WriteLine(transactionNum);
+            this.alphabet = character;
+            Console.WriteLine(alphabet);
+        }
+        public int numberAdd;
+        public int numberSub;
+        public Constructor(int numOne, int numTwo)
+        {
+            this.numberAdd = numOne + numTwo;
+            Console.WriteLine(numberAdd);
+            this.numberSub = numOne- numTwo;
+            Console.WriteLine(numberSub);
+        }
+        public Constructor(string alpha, string beta, string gamma)
+        {
+            Console.WriteLine(alpha + beta + gamma);
+        }
+        public bool input;
+        int num;
+        public Constructor(bool output, int value)
+        {
+            this.input = output;
+            Console.WriteLine(input);
+            this.num = value;
+            Console.WriteLine(num);
         }
     }
 }
--- OopsConcept/OopsConcept/Program.cs
+++ OopsConcept/OopsConcept/Program.cs
@@ -14,6 +14,10 @@ namespace OopsConcept
             name.PrintName();
             name.PrintType("Csharp");
             Constructor nonParameterised = new Constructor();
+            Constructor parameterised = new Constructor("Phoenix", 07, 'R');
+            Constructor withParameters = new Constructor(7,4);
+            Constructor withPara = new Constructor("Seven","Eight","Nine");
+            Constructor withBoolValue = new Constructor(true,77);
             //It will hold the screen until the key is pressed.
             Console.ReadKey();
         }
