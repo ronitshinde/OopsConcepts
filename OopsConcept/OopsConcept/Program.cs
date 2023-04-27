f594133023852129f5d19918ec51afc99b0bd290
ronitshinde <rony.shinde753@gmail.com>
2023-04-27 13:32:17 +0530

    [Ronit] Add : UC9-Check the implicit and explicit type casting

--- OopsConcept/OopsConcept/Program.cs
+++ OopsConcept/OopsConcept/Program.cs
@@ -7,45 +7,54 @@ using System.Threading.Tasks;
 
 namespace OopsConcept
 {
-    public class Program : TypeAbstract
+    public class Program 
     {
         const int value = 10;
         static void Main(string[] args)
         {
-            ClassName name = new ClassName();
-            //This is interface method
-            name.WithoutImplementionMethod();
-            Constructor parameter = new Constructor();
-            parameter.WithoutImplementionMethod();
-            //This is inherited class
-            ParentClass parent = new ParentClass();
-            Console.WriteLine(parent.name = "Ronit");
-            parent.TestName();
-            parent.Class();
-            //This is Enum
-            Console.WriteLine(Project.Surya);
-            int count = (int)Project.Surya;
-            Console.WriteLine(count);
-            //This is parameterised and non parameterised method.
-            name.PrintName();
-            name.PrintType("Csharp");
-            Constructor nonParameterised = new Constructor(name, 25, "David");
-            Constructor parameterised = new Constructor("Phoenix", 07, 'R');
-            Constructor withParameters = new Constructor(7, 4);
-            Constructor withPara = new Constructor("Seven", "Eight", "Nine");
-            Constructor withBoolValue = new Constructor(true, 77);
-            //This is abstract method
-            Program program = new Program();
-            program.NonAbstractMethod();
-            program.AbstractMethod();
+            //ClassName name = new ClassName();
+            ////This is interface method
+            //name.WithoutImplementionMethod();
+            //Constructor parameter = new Constructor();
+            //parameter.WithoutImplementionMethod();
+
+            ////This is inherited class
+            //ParentClass parent = new ParentClass();
+            //Console.WriteLine(parent.name = "Ronit");
+            //parent.TestName();
+            //parent.Class();
+
+            ////This is Enum
+            //Console.WriteLine(Project.Surya);
+            //int count = (int)Project.Surya;
+            //Console.WriteLine(count);
+
+            ////This is parameterised and non parameterised method.
+            //name.PrintName();
+            //name.PrintType("Csharp");
+            //Constructor nonParameterised = new Constructor(name, 25, "David");
+            //Constructor parameterised = new Constructor("Phoenix", 07, 'R');
+            //Constructor withParameters = new Constructor(7, 4);
+            //Constructor withPara = new Constructor("Seven", "Eight", "Nine");
+            //Constructor withBoolValue = new Constructor(true, 77);
+
+            ////This is abstract method
+            //Program program = new Program();
+            //program.NonAbstractMethod();
+            //program.AbstractMethod();
+
+            //This is type casting
+            TypeCasting casting = new TypeCasting();
+            casting.Casting();
+
             //It will hold the screen until the key is pressed.
             Console.ReadKey();
         }
         //It will Override keyword the base class method in the derived class.
-        public override void AbstractMethod()
-        {
-            Console.WriteLine("This is overrided/implemented abstract method");
-        }
+        //public override void AbstractMethod()
+        //{
+        //    Console.WriteLine("This is overrided/implemented abstract method");
+        //}
     }
 }
 
---
+++ OopsConcept/OopsConcept/TypeCasting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    //Type casting is used to assign a value of one data type to other data type.
+    //There are two types of type casting 1-Implicit and 2-Explicit.
+    //Implicit : Automatically converting small type to large type.
+    //char -> int -> long -> float -> double
+    //Explicit : It should be manually.
+    //Reverse of the implicit convert large to small.
+    //double -> float -> long -> int -> char
+    public class TypeCasting
+    {
+        public void Casting()
+        {
+            //This is implicit casting/Automatically converts small to large type.
+            int value = 10;
+            double doubleValue = value;
+            Console.WriteLine(value);
+            Console.WriteLine($"Implicit conversion of integer {value} to double : " +doubleValue);
+            //This is explicit casting/Manually convert large to small type.
+            double number = 10.05;
+            int integer =(int) number;
+            Console.WriteLine($"Explicit conversion of double {number} to integer : " +integer);
+        }
+    }
+}
